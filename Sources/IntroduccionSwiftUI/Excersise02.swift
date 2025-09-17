59cd69e91fd9ba861019d8f067da5d58d42b47d8
Eduardo Cavero <ecavero@gmail.com>
2025-09-16 20:18:28 -0400

    Ejercicio2

---
+++ Sources/IntroduccionSwiftUI/Excersise02.swift
@@ -0,0 +1,18 @@
+//
+//  Excersise02.swift
+//  IntroduccionSwiftUI
+//
+//  Created by user278712 on 9/16/25.
+//
+
+import SwiftUI
+
+struct Excersise02: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Excersise02()
+}
